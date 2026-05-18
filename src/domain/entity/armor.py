bee2a0182ca06f17c2a6d34edd161a2aebc245dc
RomesAll <romesky845@gmail.com>
2026-05-18 20:30:50 +0300

    fix(entity): исправить ошибку с атрибутом Armor
    Разделение атрибута durability на max_durability и current_durability.
    Ошибка заключалась в том, что при использовании метода repair было возможно
    увеличить кол-во максимальной прочности durability на болшее значение чем предполагалась.

--- src/domain/entity/armor.py
+++ src/domain/entity/armor.py
@@ -1,27 +1,32 @@
 from abc import ABC, abstractmethod
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, model_validator
 
 class Armor(ABC, BaseModel):
     model_config = ConfigDict(validate_assignment=True)
     name: str = Field(..., description='Название брони')
     endurance: int = Field(..., description='Влияния на выносливость, '
                                              'чем выше, тем тяжелее броня')
-    durability: int = Field(..., ge=0, le=320, description='Прочность брони')
+    max_durability: int = Field(..., ge=0, le=320, description='Максимальная прочность брони')
+    current_durability: int = Field(..., ge=0, description='Текущая прочность брони')
 
-    @abstractmethod
+    @model_validator(mode='after')
+    def validate_current_durability(self):
+        if self.current_durability > self.max_durability:
+            raise ValueError('Текущая прочность брони не может быть больше максимальной')
+        return self
+
     def take_damage(self, damage: int) -> None:
-        raise NotImplementedError()
+        self.current_durability -= damage
 
-    @abstractmethod
     def repair(self, amount: int) -> None:
-        raise NotImplementedError()
+        self.current_durability += amount
 
 class HeadArmor(Armor):
-    durability: int = Field(...,ge=0, le=300, description='Прочность брони для головы')
+    max_durability: int = Field(...,ge=0, le=300, description='Максимальная прочность брони для головы')
     price: int = Field(..., ge=15, le=4000, description='Цена брони для головы')
 
 class BodyArmor(Armor):
-    durability: int = Field(..., ge=0, le=320, description='Прочность брони для тела')
+    max_durability: int = Field(..., ge=0, le=320, description='Максимальная прочность брони для тела')
     price: int = Field(..., ge=0, le=7000, description='Цена брони для тела')
 
 class UniqueHeadArmor(HeadArmor):
