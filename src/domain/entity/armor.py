72e6ae771b9da6ae2fc153a75fa845b2dd5b0906
RomesAll <romesky845@gmail.com>
2026-05-18 20:12:48 +0300

    feat(entity): добавить методы для взаимодействия с броней, с классом Armor
    Добавлены контрактные методы получения уроня для брони (take_damage) и
    починки брони (repair)

--- src/domain/entity/armor.py
+++ src/domain/entity/armor.py
@@ -8,6 +8,14 @@ class Armor(ABC, BaseModel):
                                              'чем выше, тем тяжелее броня')
     durability: int = Field(..., ge=0, le=320, description='Прочность брони')
 
+    @abstractmethod
+    def take_damage(self, damage: int) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def repair(self, amount: int) -> None:
+        raise NotImplementedError()
+
 class HeadArmor(Armor):
     durability: int = Field(...,ge=0, le=300, description='Прочность брони для головы')
     price: int = Field(..., ge=15, le=4000, description='Цена брони для головы')
