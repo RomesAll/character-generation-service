3a6db56b4d8282c49e994342c388300bd5aa6be1
RomesAll <romesky845@gmail.com>
2026-05-19 21:27:27 +0300

    feat(value_object): добавить конкретные объекты характеристик

--- src/domain/value_object/stat.py
+++ src/domain/value_object/stat.py
@@ -22,22 +22,62 @@ class BaseStat:
         new_current_value = min(self.maximum, self.current + amount)
         return BaseStat(self.name, new_current_value, self.maximum, self.description)
 
-
+@dataclass(frozen=True)
+class Health(BaseStat):
+    pass
 
 @dataclass(frozen=True)
-class Stat:
-    health: int = field(default=0, metadata={'description': 'Health'})
-    armor_head: int = field(default=0, metadata={'description': 'Armor head'})
-    armor_body: int = field(default=0, metadata={'description': 'Armor body'})
-    action_points: int = field(default=0, metadata={'description': 'Action points'})
-    endurance: int = field(default=0, metadata={'description': 'Endurance'})
-    moral: int = field(default=0, metadata={'description': 'Moral'})
-    courage: int = field(default=0, metadata={'description': 'Courage'})
-    hand_scourge: int = field(default=0, metadata={'description': 'Hand scourge'})
-    shooting_skill: int = field(default=0, metadata={'description': 'Shooting skill'})
-    hand_protection: int = field(default=0, metadata={'description': 'Hand protection'})
-    shooting_protection: int = field(default=0, metadata={'description': 'Shooting protection'})
-    damage: int = field(default=0, metadata={'description': 'Damage'})
-    damage_armor: int = field(default=0, metadata={'description': 'Damage armor'})
-    head_hit_chance: int = field(default=0, metadata={'description': 'Head hit chance'})
-    visibility: int = field(default=0, metadata={'description': 'Visibility'})
+class ArmorHead(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class ArmorBody(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class ActionPoint(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class Endurance(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class Moral(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class Courage(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class HandScourge(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class ShootingSkill(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class HandProtection(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class ShootingProtection(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class Damage(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class DamageArmor(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class HeadHitChance(BaseStat):
+    pass
+
+@dataclass(frozen=True)
+class Visibility(BaseStat):
+    pass
