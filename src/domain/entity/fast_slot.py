5d40a939d937fad8029b6144bb5fd41a0c86c7e0
RomesAll <romesky845@gmail.com>
2026-05-19 17:20:21 +0300

    feat(entity): изменить класс ArmsFastSlot для слотов
    Добавлен вложенный класс ManageHandItem, экземпляр которого храниться в левой и правой руке, с помощью которого можно гибко управлять поведением рук (взятие, снятия, получение) В атрибуте hand_name (ManageHandItem) хранится текущаяя рука (левая или правая), в other_hand противоположная. Также данный вложенный класс формирует удобную иерархию для взаимодействия с методами equip и unequip. В противном случае пришлось бы дублировать эти методы для левой и правой руки.

--- src/domain/entity/fast_slot.py
+++ src/domain/entity/fast_slot.py
@@ -7,5 +7,40 @@ class ArmsFastSlot(BaseModel):
         extra='allow'
     )
 
-    left_hand: object | None = Field(None, description='Слот под левую руку')
-    right_hand: object | None = Field(None, description='Слот под правую руку')
+    left_hand: 'ArmsFastSlot.ManageHandItem | None' = Field(None, description='Слот под левую руку')
+    right_hand: 'ArmsFastSlot.ManageHandItem | None' = Field(None, description='Слот под правую руку')
+
+    def __init__(self,
+                 left_hand: Equipment | None = None,
+                 right_hand: Equipment| None = None):
+        super().__init__()
+        self.left_hand = self.ManageHandItem(hand_name='left_hand', item_in_hand=left_hand)
+        self.right_hand = self.ManageHandItem(hand_name='right_hand', item_in_hand=right_hand)
+        self.left_hand.other_hand = self.right_hand
+        self.right_hand.other_hand = self.left_hand
+
+    class ManageHandItem(BaseModel):
+        hand_name: str
+        item_in_hand: Equipment | None = None
+        other_hand: 'ArmsFastSlot.ManageHandItem | None' = None
+
+        def equip(self, item: Equipment | None = None) -> (Equipment |
+                                                           tuple[Equipment, ...] |
+                                                           tuple[None, ...] | None):
+            old_item_self: Equipment = self.item_in_hand
+            old_item_other: Equipment = self.other_hand.item_in_hand
+            if item.type_equipment == EquipmentType.TWO_HAND:
+                self.item_in_hand = item
+                self.other_hand.item_in_hand = item
+                if id(old_item_self) != id(old_item_other):
+                    return old_item_self, old_item_other
+                return old_item_self
+            if old_item_self and old_item_self.type_equipment == EquipmentType.TWO_HAND:
+                self.other_hand.item_in_hand = None
+            self.item_in_hand = item
+            return old_item_self
+
+        def unequip(self) -> (Equipment |
+                              tuple[Equipment, ...] |
+                              tuple[None, ...] | None):
+            return self.equip(None)
