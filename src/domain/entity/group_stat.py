589dca2b7055de486547dd2f03fe41415ee48807
RomesAll <romesky845@gmail.com>
2026-05-26 20:04:06 +0300

    refactor(domain): временно закомментировать неиспользуемые статы

--- src/domain/entity/group_stat.py
+++ src/domain/entity/group_stat.py
@@ -1,36 +1,39 @@
 from dataclasses import dataclass
-from src.domain.value_object.stat import (
+
+from src.domain.entity.stats import (
     Health,
-    ArmorHead,
-    ArmorBody,
-    ActionPoint,
-    Endurance,
-    Moral,
-    Courage,
-    HandScourge,
-    ShootingSkill,
-    HandProtection,
-    ShootingProtection,
-    Damage,
-    DamageArmor,
-    HeadHitChance,
-    Visibility,
+    # ArmorHead,
+    # ArmorBody,
+    # ActionPoint,
+    # Endurance,
+    # Moral,
+    # Courage,
+    HandSkill,
+    # ShootingSkill,
+    # HandProtection,
+    # ShootingProtection,
+    # Damage,
+    # DamageArmor,
+    # HeadHitChance,
+    # Visibility,
 )
+
+
 
 @dataclass
 class GroupStat:
     health: Health
-    armor_head: ArmorHead
-    armor_body: ArmorBody
-    action_points: ActionPoint
-    endurance: Endurance
-    moral: Moral
-    courage: Courage
-    hand_scourge: HandScourge
-    shooting_skill: ShootingSkill
-    hand_protection: HandProtection
-    shooting_protection: ShootingProtection
-    damage: Damage
-    damage_armor: DamageArmor
-    head_hit_chance: HeadHitChance
-    visibility: Visibility
+    # armor_head: ArmorHead
+    # armor_body: ArmorBody
+    # action_points: ActionPoint
+    # endurance: Endurance
+    # moral: Moral
+    # courage: Courage
+    hand_skill: HandSkill
+    # shooting_skill: ShootingSkill
+    # hand_protection: HandProtection
+    # shooting_protection: ShootingProtection
+    # damage: Damage
+    # damage_armor: DamageArmor
+    # head_hit_chance: HeadHitChance
+    # visibility: Visibility
