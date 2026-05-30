1aea28e30550c654288871a6fd766542cf5cb152
RomesAll <romesky845@gmail.com>
2026-05-30 23:07:24 +0300

    feat(domain): обновить импорты

--- src/domain/entity/stats/__init__.py
+++ src/domain/entity/stats/__init__.py
@@ -1,5 +1,19 @@
-from .hand_skill import HandSkill
-from .health import Health
+from .stats import (
+    Health,
+    ActionPoint,
+    Bravery,
+    Endurance,
+    Initiative,
+    MeleeDefense,
+    ShootingProtection,
+    Damage,
+    Visibility,
+    Moral,
+    HandSkill,
+    ShootingSkill,
+    DamageArmor,
+    HeadHitChance,
+)
 from src.domain.value_object.enums import Measurement, StatEnum
 from .group_stats import GroupStat
 from .base import BaseStat
@@ -8,13 +22,37 @@ MultiplierObject = tuple[int, Measurement]
 MultipliersList = list[MultiplierObject]
 
 MAPPING_STATS: dict = {
-    StatEnum.HEALTH: Health,
-    StatEnum.HAND_SKILL: HandSkill,
+    StatEnum.HEALTH : Health,
+    StatEnum.ACTION_POINT : ActionPoint,
+    StatEnum.BRAVERY : Bravery,
+    StatEnum.ENDURANCE : Endurance,
+    StatEnum.INITIATIVE : Initiative,
+    StatEnum.MELEE_DEFENSE : MeleeDefense,
+    StatEnum.SHOOTING_PROTECTION : ShootingProtection,
+    StatEnum.DAMAGE : Damage,
+    StatEnum.VISIBILITY : Visibility,
+    StatEnum.MORAL : Moral,
+    StatEnum.HAND_SKILL : HandSkill,
+    StatEnum.SHOOTING_SKILL : ShootingSkill,
+    StatEnum.DAMAGE_ARMOR : DamageArmor,
+    StatEnum.HEAD_HIT_CHANCE : HeadHitChance,
 }
 
 __all__ = [
-    'HandSkill',
     'Health',
+    'ActionPoint',
+    'Bravery',
+    'Endurance',
+    'Initiative',
+    'MeleeDefense',
+    'ShootingProtection',
+    'Damage',
+    'Visibility',
+    'Moral',
+    'HandSkill',
+    'ShootingSkill',
+    'DamageArmor',
+    'HeadHitChance',
     'GroupStat',
     'BaseStat',
     'MAPPING_STATS',
