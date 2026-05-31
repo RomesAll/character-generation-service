165afe8e2d343b181b5b9d6e8d83152df1eb0ade
RomesAll <romesky845@gmail.com>
2026-05-31 18:42:02 +0300

    refactor(domain): обновить импорты у value_object пакета

--- src/domain/value_object/__init__.py
+++ src/domain/value_object/__init__.py
@@ -1,10 +1,8 @@
 from .enums import StatEnum, BodyPart, Measurement, EquipmentType
-from .perk import GroupPerk
 
 __all__ = [
     'BodyPart',
     'EquipmentType',
-    'GroupPerk',
     'Measurement',
     'StatEnum',
 ]
