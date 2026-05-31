ba441a2897f70ce212274f0379601f2c2dd19c47
RomesAll <romesky845@gmail.com>
2026-05-31 19:43:44 +0300

    refactor(domain): резделить статы на процентные и единичные
    - Добавленый класс UnitStat обрабатывает статы, которые представляют собой целые числа
    
    - Добавленый класс PercentStat обрабатывает статы, которые представляют собой проценты
    
    * У них разные правила добавление, удаление модификаторов, поэтому они были вынесены в отдельные подклассы. Например, у статов, которые наследуются от UnitStat подсчет итогового значения стата с учетом модификаторов (перков) может осуществлятся как с помощью процента (10 ед. увеличить на 15%), так и с помощью единиц (10 ед. увеличить на 15 ед.). У наследников PercentStat мы не можем увеличить, например, "процент попадания в голову" на 10 ед., т.к. вычисления происходят в процентах. Также за базовое _basic значение у PercentStat берется current значение, т.к. нет смылса увеличить максимальное число 100% на 110% т.к. преимещества за счет перков оно не дает. У  UnitStat ровно противоположная ситуация

--- src/domain/entity/stats/base.py
+++ src/domain/entity/stats/base.py
@@ -1,40 +1,51 @@
 from abc import ABC
+
 from pydantic import BaseModel, PrivateAttr, Field, model_validator
-from typing import TYPE_CHECKING
 
 from src.domain.value_object.enums import StatEnum, Measurement
+from src.domain.value_object.perk import PerkMultiplier
 
-if TYPE_CHECKING:
-    from . import MultipliersList, MultiplierObject
 
 class BaseStat(BaseModel, ABC):
+    name: StatEnum = Field(..., frozen=True)
     current: int = Field(default=0, ge=0)
     maximum: int = Field(default=0, ge=0)
-    _name: 'StatEnum' = PrivateAttr()
     _basic: int = PrivateAttr(default=0)
-    _multipliers: 'MultipliersList' = PrivateAttr(default_factory=list)
+    _multipliers: list[PerkMultiplier] = PrivateAttr(default_factory=list)
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def validate_current_value(self):
         if self.current > self.maximum:
-            raise ValueError('Current value cannot be greater than maximum')
+            raise ValueError("Current value cannot be greater than maximum")
         return self
 
-    def append_multipliers(self, multiplier: 'MultiplierObject'):
-        self._multipliers.append(multiplier)
-        self.maximum = self.__value_stat_with_multipliers()
+    @property
+    def basic(self) -> int:
+        return self._basic
 
-    def remove_multipliers(self, multiplier: 'MultiplierObject'):
-        delete_object: 'MultiplierObject | None' = None
-        for ind, obj in enumerate(start=0, iterable=self._multipliers):
-            if obj[0] == multiplier[0] and obj[1] == multiplier[1]:
+    @property
+    def multipliers(self):
+        return self._multipliers.copy()
+
+    def append_multipliers(self, multiplier: PerkMultiplier):
+        self._multipliers.append(multiplier)
+
+    def remove_multipliers(self, multiplier: PerkMultiplier):
+        delete_object: PerkMultiplier | None = None
+        for ind, perk_multiplier in enumerate(start=0, iterable=self._multipliers):
+            if perk_multiplier == multiplier:
                 delete_object = self._multipliers.pop(ind)
                 break
         if not delete_object:
-            raise ValueError(f'Multiplier {multiplier} not found')
-        self.maximum = self.__value_stat_with_multipliers()
+            raise ValueError(f"Multiplier {multiplier} not found")
 
-    def __value_stat_with_multipliers(self) -> int:
+
+class UnitStat(BaseStat, ABC):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._basic = self.maximum
+
+    def value_stat_with_multipliers(self) -> int:
         result = self._basic
         for multiplier, measurement in self._multipliers:
             match measurement:
@@ -44,18 +55,39 @@ class BaseStat(BaseModel, ABC):
                     result = round(result * (1 + multiplier / 100))
         return result
 
+    def append_multipliers(self, multiplier: PerkMultiplier):
+        super().append_multipliers(multiplier)
+        self.maximum = self.__value_stat_with_multipliers()
+
+    def remove_multipliers(self, multiplier: PerkMultiplier):
+        super().remove_multipliers(multiplier)
+        self.maximum = self.__value_stat_with_multipliers()
+
     def level_up(self):
         self.maximum = self.maximum + 5
         self._basic = self._basic + 5
 
-    @property
-    def basic(self) -> int:
-        return self._basic
 
-    @property
-    def multipliers(self):
-        return self._multipliers
+class PercentStat(BaseStat, ABC):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._basic = self.current
 
-    @property
-    def name(self):
-        return self._name
+    def value_stat_with_multipliers(self) -> int:
+        result = self._basic
+        for multiplier, measurement in self._multipliers:
+            match measurement:
+                case Measurement.PERCENT:
+                    result += multiplier
+        return result
+
+    def append_multipliers(self, multiplier: PerkMultiplier):
+        super().append_multipliers(multiplier)
+        self.current = self.__value_stat_with_multipliers()
+
+    def remove_multipliers(self, multiplier: PerkMultiplier):
+        super().remove_multipliers(multiplier)
+        self.current = self.__value_stat_with_multipliers()
+
+    def level_up(self):
+        self.current = min(self.current + 5, 100)
