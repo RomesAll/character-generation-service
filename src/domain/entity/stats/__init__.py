d6c3385683c032bad189b4bc84a8ae3b49501123
RomesAll <romesky845@gmail.com>
2026-05-31 20:43:29 +0300

    refactor(domain): обносить импорты у пакета entity/stats

--- src/domain/entity/stats/__init__.py
+++ src/domain/entity/stats/__init__.py
@@ -1,3 +1,5 @@
+from .base import BaseStat, UnitStat, PercentStat
+from .mapping import MAPPING_STATS
 from .stats import (
     Health,
     ActionPoint,
@@ -14,29 +16,6 @@ from .stats import (
     DamageArmor,
     HeadHitChance,
 )
-from src.domain.value_object.enums import Measurement, StatEnum
-from .group_stats import GroupStat
-from .base import BaseStat
-
-MultiplierObject = tuple[int, Measurement]
-MultipliersList = list[MultiplierObject]
-
-MAPPING_STATS: dict = {
-    StatEnum.HEALTH : Health,
-    StatEnum.ACTION_POINT : ActionPoint,
-    StatEnum.BRAVERY : Bravery,
-    StatEnum.ENDURANCE : Endurance,
-    StatEnum.INITIATIVE : Initiative,
-    StatEnum.MELEE_DEFENSE : MeleeDefense,
-    StatEnum.SHOOTING_PROTECTION : ShootingProtection,
-    StatEnum.DAMAGE : Damage,
-    StatEnum.VISIBILITY : Visibility,
-    StatEnum.MORAL : Moral,
-    StatEnum.HAND_SKILL : HandSkill,
-    StatEnum.SHOOTING_SKILL : ShootingSkill,
-    StatEnum.DAMAGE_ARMOR : DamageArmor,
-    StatEnum.HEAD_HIT_CHANCE : HeadHitChance,
-}
 
 __all__ = [
     'Health',
@@ -53,9 +32,8 @@ __all__ = [
     'ShootingSkill',
     'DamageArmor',
     'HeadHitChance',
-    'GroupStat',
     'BaseStat',
-    'MAPPING_STATS',
-    'MultiplierObject',
-    'MultipliersList',
+    'UnitStat',
+    'PercentStat',
+    'MAPPING_STATS'
 ]
