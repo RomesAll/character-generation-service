f1457e3291d345925633fa99ac0643bc1541425d
RomesAll <romesky845@gmail.com>
2026-05-31 20:46:46 +0300

    style(domain): добавить пробелы

--- src/domain/entity/outfits/armor.py
+++ src/domain/entity/outfits/armor.py
@@ -5,32 +5,46 @@ from pydantic import BaseModel, Field, ConfigDict, model_validator
 
 from src.domain.value_object.enums import BodyPart
 
+
 class Armor(ABC, BaseModel):
     model_config = ConfigDict(validate_assignment=True)
-    name: str = Field(..., description='Название брони')
-    endurance: int = Field(..., description='Влияния на выносливость, '
-                                             'чем выше, тем тяжелее броня')
-    max_durability: int = Field(..., ge=0, le=320, description='Максимальная прочность брони')
-    current_durability: int = Field(..., ge=0, description='Текущая прочность брони')
+    name: str = Field(..., description="Название брони")
+    endurance: int = Field(
+        ..., description="Влияния на выносливость, " "чем выше, тем тяжелее броня"
+    )
+    max_durability: int = Field(
+        ..., ge=0, le=320, description="Максимальная прочность брони"
+    )
+    current_durability: int = Field(..., ge=0, description="Текущая прочность брони")
     body_part: BodyPart
     image: Path
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def validate_current_durability(self):
         if self.current_durability > self.max_durability:
-            raise ValueError('Текущая прочность брони не может быть больше максимальной')
+            raise ValueError(
+                "Текущая прочность брони не может быть больше максимальной"
+            )
         return self
 
     def take_damage(self, damage: int) -> None:
         self.current_durability = max(0, self.current_durability - damage)
 
     def repair(self, amount: int) -> None:
-        self.current_durability = min(self.max_durability, self.current_durability + amount)
+        self.current_durability = min(
+            self.max_durability, self.current_durability + amount
+        )
+
 
 class HeadArmor(Armor):
-    max_durability: int = Field(...,ge=0, le=300, description='Максимальная прочность брони для головы')
-    price: int = Field(..., ge=15, le=4000, description='Цена брони для головы')
+    max_durability: int = Field(
+        ..., ge=0, le=300, description="Максимальная прочность брони для головы"
+    )
+    price: int = Field(..., ge=15, le=4000, description="Цена брони для головы")
+
 
 class BodyArmor(Armor):
-    max_durability: int = Field(..., ge=0, le=320, description='Максимальная прочность брони для тела')
-    price: int = Field(..., ge=0, le=7000, description='Цена брони для тела')
+    max_durability: int = Field(
+        ..., ge=0, le=320, description="Максимальная прочность брони для тела"
+    )
+    price: int = Field(..., ge=0, le=7000, description="Цена брони для тела")
--- src/domain/entity/outfits/arms.py
+++ src/domain/entity/outfits/arms.py
@@ -1,49 +1,63 @@
 from abc import ABC
 from pydantic import BaseModel, Field, ConfigDict, model_validator
 from pathlib import Path
+
 from src.domain.value_object.enums import EquipmentType
+
 
 class Equipment(ABC, BaseModel):
     model_config = ConfigDict(validate_assignment=True)
 
-    name: str = Field(..., frozen=True, description='Название оружия')
-    type_equipment: EquipmentType = Field(..., frozen=True, description='Тип оружия (одноручный, двуручный)')
-    price: int = Field(..., ge=0, description='Цена оружия')
-    max_durability: int = Field(..., ge=0, le=100, frozen=True, description='Максимальная прочность')
-    current_durability: int = Field(..., ge=0, le=100, description='Текущая прочность')
-    peculiarities: object | None = Field(None, description='Особенности')
+    name: str = Field(..., frozen=True, description="Название оружия")
+    type_equipment: EquipmentType = Field(
+        ..., frozen=True, description="Тип оружия (одноручный, двуручный)"
+    )
+    price: int = Field(..., ge=0, description="Цена оружия")
+    max_durability: int = Field(
+        ..., ge=0, le=100, frozen=True, description="Максимальная прочность"
+    )
+    current_durability: int = Field(..., ge=0, le=100, description="Текущая прочность")
+    peculiarities: object | None = Field(None, description="Особенности")
     image: Path
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def validate_current_durability(self):
         if self.current_durability > self.max_durability:
-            raise ValueError('Текущая прочность оружия не может быть больше максимальной')
+            raise ValueError(
+                "Текущая прочность оружия не может быть больше максимальной"
+            )
         return self
 
     def take_damage(self, damage: int) -> None:
         self.current_durability = max(0, self.current_durability - damage)
 
     def repair(self, amount: int) -> None:
-        self.current_durability = min(self.max_durability, self.current_durability + amount)
+        self.current_durability = min(
+            self.max_durability, self.current_durability + amount
+        )
+
 
 class Arms(Equipment, ABC):
-    damage: tuple[int, int] = Field(..., description='Урон оружия в диапазоне')
-    ignore_armor: int = Field(..., ge=0, le=100, description='Игнорирование брони в %')
-    damage_armor: int = Field(..., ge=0, le=100, description='Урон по броне в %')
-    max_endurance: int = Field(..., description='Максимальная выносливость')
+    damage: tuple[int, int] = Field(..., description="Урон оружия в диапазоне")
+    ignore_armor: int = Field(..., ge=0, le=100, description="Игнорирование брони в %")
+    damage_armor: int = Field(..., ge=0, le=100, description="Урон по броне в %")
+    max_endurance: int = Field(..., description="Максимальная выносливость")
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def validate_damage_difficulty(self):
         if self.damage[1] - self.damage[0] < 0:
-            raise ValueError('Максимальный урон не может быть меньше минимального')
+            raise ValueError("Максимальный урон не может быть меньше минимального")
         return self
+
 
 class MeleeArms(Arms):
     pass
 
+
 class RangedArms(Arms):
-    range: int = Field(..., ge=0, description='Дальность выстрела')
+    range: int = Field(..., ge=0, description="Дальность выстрела")
+
 
 class ShieldArms(Equipment):
-    hand_protection: int = Field(..., ge=1, description='Рукопашная защита')
-    shooting_protection: int = Field(..., ge=1, description='Стрелковая защита')
+    hand_protection: int = Field(..., ge=1, description="Рукопашная защита")
+    shooting_protection: int = Field(..., ge=1, description="Стрелковая защита")
