e86b340e6bbea39180c7f03590fdecbe83a45672
RomesAll <romesky845@gmail.com>
2026-05-31 18:33:08 +0300

    style(domain): добавил отступы и переместил класс GroupPerks
    - Класс перемещен по пути */src/domain/entity/group_characteristics

--- src/domain/value_object/perk.py
+++ src/domain/value_object/perk.py
@@ -1,17 +1,10 @@
-import weakref
 from dataclasses import dataclass, field
+from src.domain.value_object.enums import Measurement, StatEnum
+from typing import NewType
 
-from src.domain.entity.stats.base import BaseStat
-from src.domain.value_object.enums import Measurement
-from src.domain.entity.stats import StatEnum
-from typing import NewType, TYPE_CHECKING
+NamePerk = NewType("NamePerk", str)
+AmountPerk = NewType("AmountPerk", int)
 
-if TYPE_CHECKING:
-    from src.domain.entity.stats import MultiplierObject
-    from src.domain.entity.characters.character import Character
-
-NamePerk = NewType('NamePerk', str)
-AmountPerk = NewType('AmountPerk', int)
 
 @dataclass(frozen=True)
 class PerkStat:
@@ -19,30 +12,8 @@ class PerkStat:
     amount: int
     measurement: Measurement
 
+
 @dataclass(frozen=True)
 class Perk:
     name: str
     stats: list[PerkStat] = field(default_factory=list)
-
-class GroupPerk:
-    def __init__(self):
-        self.perks = []
-        self.character: weakref.ref[Character] | None = None
-
-    def add(self, name: NamePerk, stats: list[tuple[StatEnum, AmountPerk, Measurement]]):
-        perk = Perk(name=name)
-        for stat, amount, measurement in stats:
-            perk.stats.append(PerkStat(stat, amount, measurement))
-            if self.character:
-                character_stat: BaseStat = self.character().stats.get_stat(stat)
-                multiplier: MultiplierObject = (amount, measurement)
-                character_stat.append_multipliers(multiplier)
-        self.perks.append(perk)
-        return self
-
-    def remove(self, name: NamePerk):
-        for perk in self.perks:
-            if perk.name == name:
-                self.perks.remove(perk)
-                return self
-        raise ValueError(f'Perk {name} not found')
