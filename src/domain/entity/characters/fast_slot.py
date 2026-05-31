50503734f3a116f0081b02801ccd9b40f7e5b6a0
RomesAll <romesky845@gmail.com>
2026-05-31 21:11:14 +0300

    refactor(domain): убрать слабые ссылки для персонажа
    - Убранные слабые ссылки могли бы привести к ошибкам

--- src/domain/entity/characters/fast_slot.py
+++ src/domain/entity/characters/fast_slot.py
@@ -1,7 +1,3 @@
-
-import weakref
-from typing import TYPE_CHECKING
-
 from pydantic import BaseModel, Field, ConfigDict
 
 from src.domain.value_object.enums import EquipmentType, BodyPart
@@ -10,47 +6,40 @@ from src.domain.entity.outfits.armor import Armor, HeadArmor, BodyArmor
 
 
 class ArmsFastSlot(BaseModel):
-    model_config = ConfigDict(
-        extra='allow',
-        arbitrary_types_allowed=True
+    model_config = ConfigDict(extra="allow", arbitrary_types_allowed=True)
+
+    left_hand: "ArmsFastSlot.ManageHandItem | None" = Field(
+        None, description="Слот под левую руку"
+    )
+    right_hand: "ArmsFastSlot.ManageHandItem | None" = Field(
+        None, description="Слот под правую руку"
     )
 
-    left_hand: 'ArmsFastSlot.ManageHandItem | None' = Field(None, description='Слот под левую руку')
-    right_hand: 'ArmsFastSlot.ManageHandItem | None' = Field(None, description='Слот под правую руку')
-    character: weakref.ref | None = Field(None)
-
-    def __init__(self,
-                 left_hand: Equipment | None = None,
-                 right_hand: Equipment| None = None,
-                 character: weakref.ref | None = None,):
+    def __init__(
+        self,
+        left_hand: Equipment | None = None,
+        right_hand: Equipment | None = None,
+    ):
         super().__init__()
         self.left_hand = self.ManageHandItem(
-            hand_name=BodyPart.LEFT_HAND,
-            item_in_hand=left_hand,
-            character=character
+            hand_name=BodyPart.LEFT_HAND, item_in_hand=left_hand
         )
         self.right_hand = self.ManageHandItem(
-            hand_name=BodyPart.RIGHT_HAND,
-            item_in_hand=right_hand,
-            character=character
+            hand_name=BodyPart.RIGHT_HAND, item_in_hand=right_hand
         )
         self.left_hand.other_hand = self.right_hand
         self.right_hand.other_hand = self.left_hand
 
     class ManageHandItem(BaseModel):
-        model_config = ConfigDict(
-            extra='allow',
-            arbitrary_types_allowed=True
-        )
+        model_config = ConfigDict(extra="allow", arbitrary_types_allowed=True)
 
         hand_name: BodyPart
         item_in_hand: Equipment | None = None
-        other_hand: 'ArmsFastSlot.ManageHandItem | None' = Field(None, repr=False)
-        character: weakref.ref | None = Field(None)
+        other_hand: "ArmsFastSlot.ManageHandItem | None" = Field(None, repr=False)
 
-        def equip(self, item: Equipment | None = None) -> (Equipment |
-                                                           tuple[Equipment, ...] |
-                                                           tuple[None, ...] | None):
+        def equip(
+            self, item: Equipment | None = None
+        ) -> Equipment | tuple[Equipment, ...] | tuple[None, ...] | None:
             old_item_self: Equipment = self.item_in_hand
             old_item_other: Equipment = self.other_hand.item_in_hand
             if item.type_equipment == EquipmentType.TWO_HAND:
@@ -62,65 +51,48 @@ class ArmsFastSlot(BaseModel):
             if old_item_self and old_item_self.type_equipment == EquipmentType.TWO_HAND:
                 self.other_hand.item_in_hand = None
             self.item_in_hand = item
-            if self.character:
-                if self.hand_name == BodyPart.LEFT_HAND:
-                    self.character()._left_hand = weakref.ref(item)
-                elif self.hand_name == BodyPart.RIGHT_HAND:
-                    self.character()._right_hand = weakref.ref(item)
             return old_item_self
 
-        def unequip(self) -> (Equipment |
-                              tuple[Equipment, ...] |
-                              tuple[None, ...] | None):
+        def unequip(
+            self,
+        ) -> Equipment | tuple[Equipment, ...] | tuple[None, ...] | None:
             return self.equip(None)
 
 
 class ArmorFastSlot(BaseModel):
-    model_config = ConfigDict(
-        extra='allow',
-        arbitrary_types_allowed=True
+    model_config = ConfigDict(extra="allow", arbitrary_types_allowed=True)
+
+    head: "ArmorFastSlot.ManageArmorItem | None" = Field(
+        None, description="Слот для головы"
+    )
+    body: "ArmorFastSlot.ManageArmorItem | None" = Field(
+        None, description="Слот для тела"
     )
 
-    head: 'ArmorFastSlot.ManageArmorItem | None' = Field(None, description='Слот для головы')
-    body: 'ArmorFastSlot.ManageArmorItem | None' = Field(None, description='Слот для тела')
-    character: weakref.ref | None = Field(None)
-
-    def __init__(self,
-                 head: HeadArmor | None = None,
-                 body: BodyArmor | None = None,
-                 character: weakref.ref | None = None,):
+    def __init__(
+        self,
+        head: HeadArmor | None = None,
+        body: BodyArmor | None = None,
+    ):
         super().__init__()
         self.head = self.ManageArmorItem(
-            body_part_name=BodyPart.HEAD,
-            armor=head,
-            character=character
+            body_part_name=BodyPart.HEAD, armor=head
         )
         self.body = self.ManageArmorItem(
-            body_part_name=BodyPart.BODY,
-            armor=body,
-            character=character
+            body_part_name=BodyPart.BODY, armor=body
         )
 
     class ManageArmorItem(BaseModel):
-        model_config = ConfigDict(
-            extra='allow',
-            arbitrary_types_allowed=True
-        )
+        model_config = ConfigDict(extra="allow", arbitrary_types_allowed=True)
 
         body_part_name: BodyPart
         armor: Armor | None = None
-        character: weakref.ref | None = Field(None)
 
         def equip(self, new_armor: Armor | None = None) -> Armor | None:
             if new_armor and self.body_part_name != new_armor.body_part:
-                raise ValueError('Броня не подходит под данную часть тела')
+                raise ValueError("Броня не подходит под данную часть тела")
             old_armor = self.armor
             self.armor = new_armor
-            if self.character:
-                if self.body_part_name == BodyPart.HEAD:
-                    self.character()._head = weakref.ref(new_armor)
-                elif self.body_part_name == BodyPart.BODY:
-                    self.character()._body = weakref.ref(new_armor)
             return old_armor
 
         def unequip(self) -> Armor | None:
