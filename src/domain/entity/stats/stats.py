da4d59cdd21556339017590664a80efe3a144d1a
RomesAll <romesky845@gmail.com>
2026-06-02 12:31:37 +0300

    fix(domain): убрать нижнее подчер. у атрибута name и изменить ограничения у статов
    - Исправлена ошибка некорректного уровня доступа у атрибута вместо _name необходимо name
    
    - Исправлена ошибка неправильного ограничения вместо ge (больше или равно) на le (меньше или равно), ge приводило к тому, что здоровье, например должно было быть больше 200 (а это в рамках пред. области некорректно)

--- src/domain/entity/stats/stats.py
+++ src/domain/entity/stats/stats.py
@@ -4,84 +4,84 @@ from src.domain.value_object import StatEnum
 
 
 class Health(UnitStat):
-    current: int = Field(default=0, ge=150)
-    maximum: int = Field(default=0, ge=150)
-    _name: "StatEnum" = StatEnum.HEALTH
+    current: int = Field(default=0, le=200)
+    maximum: int = Field(default=0, le=200)
+    name: "StatEnum" = StatEnum.HEALTH
 
 
 class ActionPoint(UnitStat):
-    current: int = Field(default=0, ge=15)
-    maximum: int = Field(default=0, ge=15)
-    _name: "StatEnum" = StatEnum.ACTION_POINT
+    current: int = Field(default=0, le=15)
+    maximum: int = Field(default=0, le=15)
+    name: "StatEnum" = StatEnum.ACTION_POINT
 
 
 class Bravery(UnitStat):
-    current: int = Field(default=0, ge=100)
-    maximum: int = Field(default=0, ge=100)
-    _name: "StatEnum" = StatEnum.BRAVERY
+    current: int = Field(default=0, le=150)
+    maximum: int = Field(default=0, le=150)
+    name: "StatEnum" = StatEnum.BRAVERY
 
 
 class Endurance(UnitStat):
-    current: int = Field(default=0, ge=73)
-    maximum: int = Field(default=0, ge=73)
-    _name: "StatEnum" = StatEnum.ENDURANCE
+    current: int = Field(default=0, le=80)
+    maximum: int = Field(default=0, le=80)
+    name: "StatEnum" = StatEnum.ENDURANCE
 
 
 class Initiative(UnitStat):
-    current: int = Field(default=0, ge=100)
-    maximum: int = Field(default=0, ge=100)
-    _name: "StatEnum" = StatEnum.INITIATIVE
+    current: int = Field(default=0, le=120)
+    maximum: int = Field(default=0, le=120)
+    name: "StatEnum" = StatEnum.INITIATIVE
 
 
 class MeleeDefense(UnitStat):
-    current: int = Field(default=0, ge=100)
-    maximum: int = Field(default=0, ge=100)
-    _name: "StatEnum" = StatEnum.MELEE_DEFENSE
+    current: int = Field(default=0, le=150)
+    maximum: int = Field(default=0, le=150)
+    name: "StatEnum" = StatEnum.MELEE_DEFENSE
 
 
 class ShootingProtection(UnitStat):
-    current: int = Field(default=0, ge=100)
-    maximum: int = Field(default=0, ge=100)
-    _name: "StatEnum" = StatEnum.SHOOTING_PROTECTION
+    current: int = Field(default=0, le=150)
+    maximum: int = Field(default=0, le=150)
+    name: "StatEnum" = StatEnum.SHOOTING_PROTECTION
 
 
 class Damage(UnitStat):
-    current: int = Field(default=0, ge=200)
-    maximum: int = Field(default=0, ge=200)
-    _name: "StatEnum" = StatEnum.DAMAGE
+    current: int = Field(default=0, le=220)
+    maximum: int = Field(default=0, le=220)
+    name: "StatEnum" = StatEnum.DAMAGE
 
 
 class Visibility(UnitStat):
-    current: int = Field(default=0, ge=10)
-    maximum: int = Field(default=0, ge=10)
-    _name: "StatEnum" = StatEnum.VISIBILITY
+    current: int = Field(default=0, le=10)
+    maximum: int = Field(default=0, le=10)
+    name: "StatEnum" = StatEnum.VISIBILITY
 
 
 class Moral(UnitStat):
-    current: int = Field(default=0, ge=100)
-    maximum: int = Field(default=0, ge=100)
-    _name: "StatEnum" = StatEnum.MORAL
+    current: int = Field(default=0, le=130)
+    maximum: int = Field(default=0, le=130)
+    name: "StatEnum" = StatEnum.MORAL
 
 
 class HandSkill(PercentStat):
-    current: int = Field(default=0, ge=100)
-    maximum: int = Field(default=0, ge=100)
-    _name: "StatEnum" = StatEnum.HAND_SKILL
+    current: int = Field(default=0, le=100)
+    maximum: int = Field(default=0, le=100)
+    name: "StatEnum" = StatEnum.HAND_SKILL
 
 
 class ShootingSkill(PercentStat):
-    current: int = Field(default=0, ge=100)
-    maximum: int = Field(default=0, ge=100)
-    _name: "StatEnum" = StatEnum.SHOOTING_SKILL
+    current: int = Field(default=0, le=100)
+    maximum: int = Field(default=0, le=100)
+    name: "StatEnum" = StatEnum.SHOOTING_SKILL
 
 
 class DamageArmor(PercentStat):
-    current: int = Field(default=0, ge=200)
-    maximum: int = Field(default=0, ge=200)
-    _name: "StatEnum" = StatEnum.DAMAGE_ARMOR
+    current: int = Field(default=0, le=200)
+    maximum: int = Field(default=0, le=200)
+    name: "StatEnum" = StatEnum.DAMAGE_ARMOR
 
 
 class HeadHitChance(PercentStat):
-    current: int = Field(default=0, ge=90)
-    maximum: int = Field(default=0, ge=90)
-    _name: "StatEnum" = StatEnum.HEAD_HIT_CHANCE
+    current: int = Field(default=0, le=100)
+    maximum: int = Field(default=0, le=100)
+    name: "StatEnum" = StatEnum.HEAD_HIT_CHANCE
