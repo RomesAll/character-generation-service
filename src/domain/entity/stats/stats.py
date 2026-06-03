1c276d3fc04eea5a8dc36457ebab35250f52b266
RomesAll <romesky845@gmail.com>
2026-06-03 19:17:50 +0300

    fix(domain): добавить ограничения на меньше или равно в атрибутах класса статов

--- src/domain/entity/stats/stats.py
+++ src/domain/entity/stats/stats.py
@@ -4,84 +4,84 @@ from src.domain.value_object import StatEnum
 
 
 class Health(UnitStat):
-    current: int = Field(default=0, le=200)
-    maximum: int = Field(default=0, le=200)
+    current: int = Field(default=0, ge=0, le=200)
+    maximum: int = Field(default=0, ge=0, le=200)
     name: "StatEnum" = StatEnum.HEALTH
 
 
 class ActionPoint(UnitStat):
-    current: int = Field(default=0, le=15)
-    maximum: int = Field(default=0, le=15)
+    current: int = Field(default=0, ge=0, le=15)
+    maximum: int = Field(default=0, ge=0, le=15)
     name: "StatEnum" = StatEnum.ACTION_POINT
 
 
 class Bravery(UnitStat):
-    current: int = Field(default=0, le=150)
-    maximum: int = Field(default=0, le=150)
+    current: int = Field(default=0, ge=0, le=150)
+    maximum: int = Field(default=0, ge=0, le=150)
     name: "StatEnum" = StatEnum.BRAVERY
 
 
 class Endurance(UnitStat):
-    current: int = Field(default=0, le=80)
-    maximum: int = Field(default=0, le=80)
+    current: int = Field(default=0, ge=0, le=80)
+    maximum: int = Field(default=0, ge=0, le=80)
     name: "StatEnum" = StatEnum.ENDURANCE
 
 
 class Initiative(UnitStat):
-    current: int = Field(default=0, le=120)
-    maximum: int = Field(default=0, le=120)
+    current: int = Field(default=0, ge=0, le=120)
+    maximum: int = Field(default=0, ge=0, le=120)
     name: "StatEnum" = StatEnum.INITIATIVE
 
 
 class MeleeDefense(UnitStat):
-    current: int = Field(default=0, le=150)
-    maximum: int = Field(default=0, le=150)
+    current: int = Field(default=0, ge=0, le=150)
+    maximum: int = Field(default=0, ge=0, le=150)
     name: "StatEnum" = StatEnum.MELEE_DEFENSE
 
 
 class ShootingProtection(UnitStat):
-    current: int = Field(default=0, le=150)
-    maximum: int = Field(default=0, le=150)
+    current: int = Field(default=0, ge=0, le=150)
+    maximum: int = Field(default=0, ge=0, le=150)
     name: "StatEnum" = StatEnum.SHOOTING_PROTECTION
 
 
 class Damage(UnitStat):
-    current: int = Field(default=0, le=220)
-    maximum: int = Field(default=0, le=220)
+    current: int = Field(default=0, ge=0, le=220)
+    maximum: int = Field(default=0, ge=0, le=220)
     name: "StatEnum" = StatEnum.DAMAGE
 
 
 class Visibility(UnitStat):
-    current: int = Field(default=0, le=10)
-    maximum: int = Field(default=0, le=10)
+    current: int = Field(default=0, ge=0, le=10)
+    maximum: int = Field(default=0, ge=0, le=10)
     name: "StatEnum" = StatEnum.VISIBILITY
 
 
 class Moral(UnitStat):
-    current: int = Field(default=0, le=130)
-    maximum: int = Field(default=0, le=130)
+    current: int = Field(default=0, ge=0, le=130)
+    maximum: int = Field(default=0, ge=0, le=130)
     name: "StatEnum" = StatEnum.MORAL
 
 
 class HandSkill(PercentStat):
-    current: int = Field(default=0, le=100)
-    maximum: int = Field(default=0, le=100)
+    current: int = Field(default=0, ge=0, le=100)
+    maximum: int = Field(default=100, ge=0, le=100)
     name: "StatEnum" = StatEnum.HAND_SKILL
 
 
 class ShootingSkill(PercentStat):
-    current: int = Field(default=0, le=100)
-    maximum: int = Field(default=0, le=100)
+    current: int = Field(default=0, ge=0, le=100)
+    maximum: int = Field(default=100, ge=0, le=100)
     name: "StatEnum" = StatEnum.SHOOTING_SKILL
 
 
 class DamageArmor(PercentStat):
-    current: int = Field(default=0, le=200)
-    maximum: int = Field(default=0, le=200)
+    current: int = Field(default=0, ge=0, le=200)
+    maximum: int = Field(default=200, ge=0,le=200)
     name: "StatEnum" = StatEnum.DAMAGE_ARMOR
 
 
 class HeadHitChance(PercentStat):
-    current: int = Field(default=0, le=100)
-    maximum: int = Field(default=0, le=100)
+    current: int = Field(default=0, ge=0, le=100)
+    maximum: int = Field(default=100, ge=0, le=100)
     name: "StatEnum" = StatEnum.HEAD_HIT_CHANCE
