2a27a3e0da1534594356d2a47c0e1406f8edd7a5
RomesAll <romesky845@gmail.com>
2026-06-03 19:40:06 +0300

    feat(domain): добавить изменение мод. при добелвении/удалении перка

--- src/domain/entity/group_characteristics/group_perks.py
+++ src/domain/entity/group_characteristics/group_perks.py
@@ -1,9 +1,15 @@
+import copy
 import weakref
 from dataclasses import dataclass, field
 from typing import NewType, Literal
 
 from src.domain.value_object.enums import Measurement, StatEnum
 from src.domain.value_object.perk import Perk, PerkMultiplier
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.domain.entity.characters import Character
+    from src.domain.entity.stats import BaseStat
 
 NamePerk = NewType("NamePerk", str)
 AmountPerk = NewType("AmountPerk", int)
@@ -12,10 +18,14 @@ AmountPerk = NewType("AmountPerk", int)
 @dataclass
 class GroupPerk:
     _perks: list[Perk] = field(default_factory=list)
+    _character: weakref.ref['Character'] | None = field(default=None, init=False)
 
     @property
     def perks(self) -> list[Perk]:
-        return self._perks.copy()
+        return copy.deepcopy(self._perks)
+
+    def get_copy(self) -> 'GroupPerk':
+        return copy.deepcopy(self)
 
     def add(
         self, name: NamePerk, stats: list[tuple[StatEnum, AmountPerk, Measurement]]
@@ -32,11 +42,17 @@ class GroupPerk:
                 measurement=measurement
             ))
         self._perks.append(perk)
+        if self._character:
+            for multiplier in perk.multipliers:
+                self._character().stats.append_multipliers(multiplier)
         return self
 
     def remove(self, name: NamePerk):
         for perk in self._perks:
             if perk.name == name:
+                if self._character:
+                    for multiplier in perk.multipliers:
+                        self._character().stats.remove_multipliers(multiplier)
                 self._perks.remove(perk)
                 return self
         raise ValueError(f"Perk {name} not found")
@@ -54,3 +70,13 @@ class GroupPerk:
         if default is None:
             return None
         raise ValueError('No perk found')
+
+    def add_all_multiplier(self):
+        for perk in self._perks:
+            self.add_multiplier(perk=perk)
+
+    def add_multiplier(self, perk: Perk):
+        for multiplier in perk.multipliers:
+            if self._character:
+                stat_obj: 'BaseStat' = self._character().stats.get_stat(multiplier.stat)
+                stat_obj.append_multipliers(multiplier)
